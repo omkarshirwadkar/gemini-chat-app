60cb5c8c0a4de766daaf397881fb87476d238274
Omkar-Shirwadkar <omi.shirwadkar@gmail.com>
2025-04-15 00:49:29 +0530

    gemini chat app

---
+++ gemini-chat/src/main/java/com/ai/gemini_chat/AiController.java
@@ -0,0 +1,27 @@
+package com.ai.gemini_chat;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/qna")
+public class AiController {
+    private final QnaService qnaService;
+    public AiController(QnaService qnaService) {
+        this.qnaService = qnaService;
+    }
+    @PostMapping("/ask")
+    public ResponseEntity<String> askQuestion(@RequestBody Map <String, String> payload) {
+        String question = payload.get("question");
+        String answer = qnaService.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
+}
